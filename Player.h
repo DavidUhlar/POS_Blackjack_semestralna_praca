a84d4c6e76d06183367b4228e18866d69a2b13b7
DavidUhlar <dadododo02@gmail.com>
2024-01-02 10:37:53 +0100

    player and dealer

---
+++ Player.h
@@ -0,0 +1,21 @@
+//
+// Created by dadod on 02.01.2024.
+//
+
+#ifndef SEMESTRALKA_PLAYER_H
+#define SEMESTRALKA_PLAYER_H
+
+
+class Player {
+    public:
+        Player();
+
+        ~Player();
+
+    private:
+        int balance;
+
+};
+
+
+#endif //SEMESTRALKA_PLAYER_H
